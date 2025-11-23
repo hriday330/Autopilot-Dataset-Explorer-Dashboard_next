871006f1cfe73d5375611850bc3e0947cc061bd6
hriday330 <hriday2004@outlook.com>
2025-11-22 23:17:57 -0800

    fix/disable create dataset name on empty name

--- dataset_explorer/components/DatasetCreation.tsx
+++ dataset_explorer/components/DatasetCreation.tsx
@@ -11,19 +11,24 @@ function DatasetCreation({
   setNewName,
   onCreate,
 }: DatasetCreationProps) {
+  const isDisabled = !newName.trim();
   return (
     <div className="mb-6">
       <div className="text-sm text-[#A3A3A3] mb-2">Create a new dataset</div>
 
-      <div className="flex gap-2">
+      <div className="flex gap-2 items-center">
         <input
           value={newName}
           onChange={(e) => setNewName(e.target.value)}
-          placeholder="dataset-name"
+          placeholder="Dataset Name"
           className="bg-transparent border border-[#1F1F1F] px-3 py-2 rounded text-white"
         />
 
-        <Button className="bg-[#E82127]" onClick={onCreate}>
+        <Button
+          className="bg-[#E82127]"
+          onClick={onCreate}
+          disabled={isDisabled}
+        >
           Create
         </Button>
       </div>
